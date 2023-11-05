f064c2253e43b28ca712212bf5d1a3e7131d2555
Khaled Qasim Len <khaled.qasim183@gmail.com>
2023-11-05 21:46:40 +0000

    Implemented HTTP/HTTPS check
    Alongside the test for checking if the server is online, we will check for the implementation of HTTPS

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import helperFunctions.runLinuxCommand as runLinuxCommand
 import testClasses.isOnline as isOnline
-
+import sys
 
 
 if __name__ == '__main__':
- print(isOnline.isOnline("https://www.google.com"))
+    # Domain will be provided as the first command line argument, however for now it is hardcoded for testing purposes
+    # domain = sys.argv[1]
+    domain = "httpforever.com"
+    isOnline.CheckOnlineAndHttp(domain)
