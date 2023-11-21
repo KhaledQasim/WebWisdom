75fb8eefcf8b41c2efeee85051467ec4e24b45eb
Khaled Qasim Kraken <khaled.qasim183@gmail.com>
2023-11-21 12:29:14 +0000

    Changed the Logo

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,5 +1,5 @@
-import EyeLogo from "../assets/eye-clean.png";
-import Pupil from "../assets/pupil.png";
+import EyeLogo from "../assets/oig-clean.png";
+import Pupil from "../assets/white.png";
 
 const isMobile =
   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|windows phone|playbook|silk|(android(?!.*mobile))/i.test(
@@ -10,40 +10,64 @@ function pupilFollow() {
   /**
    * This function makes the pupil follow the cursor
    */
+  const eye = document.getElementById("Eye-Socket");
+  const pupil = document.getElementById("pupil");
+  const scaleFactor = 0.15;
+  const eyeRect = eye.getBoundingClientRect();
+
+  const eyeCenterX = eyeRect.left + eyeRect.width / 2;
+  const eyeCenterY = eyeRect.top + eyeRect.height / 2;
+  const maxDistance = Math.min(eyeRect.width / 4, eyeRect.height / 4);
   document.addEventListener("mousemove", function (event) {
     // Follow mouse for pupil
-    const eye = document.getElementById("Eye-Socket");
-    const pupil = document.getElementById("pupil");
-    
-
-    const scaleFactor = 0.29;
-
-    const eyeRect = eye.getBoundingClientRect();
-    console.log(1,eyeRect.width);
-    const eyeCenterX = eyeRect.left + eyeRect.width / 2;
-    const eyeCenterY = eyeRect.top + eyeRect.height / 2;
 
     const angle = Math.atan2(
       event.clientY - eyeCenterY,
       event.clientX - eyeCenterX
     );
-    const maxDistance = Math.min(eyeRect.width / 4, eyeRect.height / 4);
+
     const pupilX = scaleFactor * maxDistance * Math.cos(angle);
     const pupilY = scaleFactor * maxDistance * Math.sin(angle);
 
-  
-    pupil.style.transform = `translate(-44%,-70%) translate(${pupilX}px, ${pupilY}px)`;
-
-
+    pupil.style.transform = `translate(-50%,-66%) translate(${pupilX}px, ${pupilY}px)`;
   });
 }
+
+// function angle(cx, cy, ex, ey) {
+//   const dy = ey - cy;
+//   const dx = ex - cx;
+//   const rad = Math.atan2(dy, dx);
+//   const deg = rad * (180 / Math.PI);
+//   return deg;
+// }
+
+// function pupilFollow2() {
+//   const eye = document.getElementById("Eye-Socket");
+//   const pupil = document.getElementById("pupil");
+
+//   const eyeRect = eye.getBoundingClientRect();
+
+//   const eyeCenterX = eyeRect.left + eyeRect.width / 2;
+//   const eyeCenterY = eyeRect.top + eyeRect.height / 2;
+
+//   document.addEventListener("mousemove", function (event) {
+//     // Follow mouse for pupil
+
+//     const mouseX = event.clientX;
+//     const mouseY = event.clientY;
+
+//     const angleDeg = angle(mouseX, mouseY, eyeCenterX, eyeCenterY);
+
+//     pupil.style.transform = `rotate(${180+angleDeg}deg))`;
+//   });
+// }
 
 function pupilAutoMovement() {
   setTimeout(() => {
     const eye = document.getElementById("Eye-Socket");
     const pupil = document.getElementById("pupil");
-    const pupilDot = document.getElementById("pupil-dot");
-    const scaleFactor = 0.29;
+
+    const scaleFactor = 0.15;
 
     const eyeRect = eye.getBoundingClientRect();
 
@@ -53,11 +77,12 @@ function pupilAutoMovement() {
     const pupilX = scaleFactor * maxDistance * Math.cos(randomAngle);
     const pupilY = scaleFactor * maxDistance * Math.sin(randomAngle);
 
-    pupil.style.transform = `translate(-44%,-70%) translate(${pupilX}px, ${pupilY}px)`;
-    pupilDot.style.transform = `translate(-44%,-70%) translate(${pupilX}px, ${pupilY}px)`;
+    pupil.style.transform = `translate(-50%,-66%) translate(${pupilX}px, ${pupilY}px)`;
+
     requestAnimationFrame(pupilAutoMovement);
-  }, 1000);
+  }, 700);
 }
+
 if (isMobile) {
   // Code for mobile devices
   pupilAutoMovement();
@@ -67,6 +92,7 @@ if (isMobile) {
     pupilFollow();
   }, 500);
 }
+
 function Hero() {
   return (
     <div className="hero bg-base-100">
@@ -83,9 +109,8 @@ function Hero() {
             src={Pupil}
             alt="Pupil"
             // transform -translate-x-1/2 -translate-y-1/2
-            className="md:w-[4.5rem] md:h-[4.5rem] w-9 h-9 absolute top-1/2 left-1/2 translate-x-[-44%] translate-y-[-70%] rounded-full"
+            className="md:w-7 md:h-16 w-3 h-9 absolute top-1/2 left-1/2  translate-x-[-50%] translate-y-[-66%] rounded-full"
           />
-         
         </div>
         <div className="space-y-5">
           <h1 className="text-5xl font-bold  tracking-normal ">
